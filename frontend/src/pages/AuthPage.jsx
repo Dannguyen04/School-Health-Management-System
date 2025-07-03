3d155b751b88e518f17b1b6d5e29b630da187dd7
Nguyễn Thế Văn <thevann64@gmail.com>
2025-07-03 07:57:24 +0700

    fix giao dien login

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -4,14 +4,20 @@ import { AuthTemplate } from "../components/auth/AuthTemplate";
 
 const AuthPage = () => {
     const [open, setOpen] = useState(true);
+    const [signIn, setSignIn] = useState(true);
     const navigate = useNavigate();
 
     const handleClose = () => {
         setOpen(false);
+        if (window.innerWidth <= 768) {
+            setSignIn(false);
+        } else {
+            setSignIn(true);
+        }
         navigate("/");
     };
 
-    return <AuthTemplate isOpen={open} onCloseModal={handleClose} />;
+    return <AuthTemplate isOpen={open} onCloseModal={handleClose} signIn={signIn} setSignIn={setSignIn} />;
 };
 
 export default AuthPage;
