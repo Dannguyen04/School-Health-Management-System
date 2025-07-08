497be3f6698997a50555ad20df50bcb05a483624
nqvinh1103 <162325959+nqvinh1103@users.noreply.github.com>
2025-07-09 01:08:16 +0700

    fix route from /user to /parent (#74)
    * vinh
    
    * vinh
    
    * Merge branch 'adminFeature' of https://github.com/nqvinh1103/School-Health-Management-System into frontendFeature
    
    * fix
    
    * crud vaccine
    
    * crud vaccine, add healthprofile cho nurse
    
    * Merge branch 'master' of https://github.com/nqvinh1103/School-Health-Management-System into frontendFeature
    
    * get healthprofile, send medicine
    
    * getVaccineSchedule
    
    * fix create vaccine
    
    * fix css
    
    * Merge branch 'frontendFeature' of https://github.com/nqvinh1103/School-Health-Management-System into frontendFeature
    
    * fix css p2
    
    * Merge branch 'master' of https://github.com/nqvinh1103/School-Health-Management-System into frontendFeature
    
    * update validate
    
    * css
    
    * Merge branch 'master' of https://github.com/nqvinh1103/School-Health-Management-System into frontendFeature
    
    * update
    
    * Merge branch 'master' of https://github.com/nqvinh1103/School-Health-Management-System into frontendFeature
    
    * update
    
    * update
    
    * update
    
    * update homepage
    
    * send medicine
    
    * fix
    
    * update
    
    * Merge branch 'master' of https://github.com/nqvinh1103/School-Health-Management-System into frontendFeature
    
    * fix
    
    * update front
    
    * update fe
    
    * update
    
    * fix fe
    
    * update
    
    * chill
    
    * update
    
    * update
    
    * fix
    
    * update again
    
    * update againn
    
    * fix
    
    * done tasklist
    
    * update
    
    * update code
    
    * update healthProfile, giao diện gửi thuốc
    
    * Merge branch 'master' of https://github.com/nqvinh1103/School-Health-Management-System into frontendFeature
    
    * update
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: DucNDC1810 <damchanduc1810@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import {
-    Navigate,
-    Route,
-    BrowserRouter as Router,
-    Routes,
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
 } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import AdminDashboard from "./components/admin/Dashboard";
@@ -16,6 +16,7 @@ import HealthReports from "./components/manager/HealthReports";
 import StudentList from "./components/manager/StudentList";
 import VaccinationCampaigns from "./components/manager/VaccinationCampaigns";
 import VaccineManagement from "./components/manager/VaccineManagement";
+import BlogManagement from "./components/nurse/BlogManagement";
 import ConfirmedMedicines from "./components/nurse/ConfirmedMedicines";
 import Dashboard from "./components/nurse/Dashboard";
 import HealthCheckups from "./components/nurse/HealthCheckups";
@@ -24,170 +25,138 @@ import MedicalInventory from "./components/nurse/MedicalInventory";
 import StudentHealthProfile from "./components/nurse/StudentHealthProfile";
 import StudentTreatment from "./components/nurse/StudentTreatment";
 import Vaccination from "./components/nurse/Vaccination";
-import BlogManagement from "./components/nurse/BlogManagement";
 import About from "./components/parent/About";
+import BlogDetail from "./components/parent/BlogDetail";
 import Doctors from "./components/parent/Doctors";
 import Services from "./components/parent/Services";
-import BlogDetail from "./components/parent/BlogDetail";
 import ScrollToTop from "./components/ScrollToTop";
 import UserProfile from "./components/shared/UserProfile";
 import VaccineConsentForm from "./components/user/ConsentForms";
 import HealthCheckupResults from "./components/user/HealthCheckupResults";
 import HealthProfile from "./components/user/HealthProfile";
-import MedicineInfo from "./components/user/MedicineInfo";
 import MedicalSchedule from "./components/user/MedicalSchedule";
-import VaccinationHistory from "./pages/VaccinationHistory";
+import MedicineInfo from "./components/user/MedicineInfo";
 import AdminLayout from "./layouts/Admin";
 import ManagerLayout from "./layouts/Manager";
 import NurseLayout from "./layouts/Nurse";
 import User from "./layouts/User";
-import AuthPage, { ForgotPassword } from "./pages/AuthPage";
+import AuthPage from "./pages/AuthPage";
 import Homepage from "./pages/Homepage";
+import MedicalEvents from "./pages/MedicalEvents";
+import VaccinationHistory from "./pages/VaccinationHistory";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import RoleBaseRoutes from "./utils/RoleBaseRoutes";
-import MedicalEvents from "./pages/MedicalEvents";
 
 const App = () => {
-    return (
-        <Router>
-            <ScrollToTop />
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/auth" element={<AuthPage />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/blog/:id" element={<BlogDetail />} />
-                {/* Nurse Routes */}
-                <Route
-                    path="/nurse"
-                    element={
-                        <PrivateRoutes>
-                            <RoleBaseRoutes requiredRole={["SCHOOL_NURSE"]}>
-                                <NurseLayout />
-                            </RoleBaseRoutes>
-                        </PrivateRoutes>
-                    }
-                >
-                    <Route index element={<Dashboard />} />
-                    <Route
-                        path="student-health-profile"
-                        element={<StudentHealthProfile />}
-                    />
-                    <Route
-                        path="medical-inventory"
-                        element={<MedicalInventory />}
-                    />
-                    <Route
-                        path="student-treatment"
-                        element={<StudentTreatment />}
-                    />
-                    <Route
-                        path="vaccination-report"
-                        element={<Vaccination />}
-                    />
-                    <Route
-                        path="health-checkups"
-                        element={<HealthCheckups />}
-                    />
-                    <Route
-                        path="confirmed-medicines"
-                        element={<ConfirmedMedicines />}
-                    />
-                    <Route
-                        path="medical-event"
-                        element={<MedicalEventReport />}
-                    />
-                    <Route path="profile" element={<UserProfile />} />
-                    <Route
-                        path="blog-management"
-                        element={<BlogManagement />}
-                    />
-                </Route>
+  return (
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
+        {/* Nurse Routes */}
+        <Route
+          path="/nurse"
+          element={
+            <PrivateRoutes>
+              <RoleBaseRoutes requiredRole={["SCHOOL_NURSE"]}>
+                <NurseLayout />
+              </RoleBaseRoutes>
+            </PrivateRoutes>
+          }
+        >
+          <Route index element={<Dashboard />} />
+          <Route
+            path="student-health-profile"
+            element={<StudentHealthProfile />}
+          />
+          <Route path="medical-inventory" element={<MedicalInventory />} />
+          <Route path="student-treatment" element={<StudentTreatment />} />
+          <Route path="vaccination-report" element={<Vaccination />} />
+          <Route path="health-checkups" element={<HealthCheckups />} />
+          <Route path="confirmed-medicines" element={<ConfirmedMedicines />} />
+          <Route path="medical-event" element={<MedicalEventReport />} />
+          <Route path="profile" element={<UserProfile />} />
+          <Route path="blog-management" element={<BlogManagement />} />
+        </Route>
 
-                {/* Admin Routes */}
-                <Route
-                    path="/admin"
-                    element={
-                        <PrivateRoutes>
-                            <RoleBaseRoutes requiredRole={["ADMIN"]}>
-                                <AdminLayout />
-                            </RoleBaseRoutes>
-                        </PrivateRoutes>
-                    }
-                >
-                    <Route index element={<AdminDashboard />} />
-                    <Route path="users" element={<UserManagement />} />
-                    <Route path="students" element={<StudentManagement />} />
-                    <Route path="parents" element={<ParentManagement />} />
-                    <Route path="profile" element={<UserProfile />} />
-                </Route>
+        {/* Admin Routes */}
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoutes>
+              <RoleBaseRoutes requiredRole={["ADMIN"]}>
+                <AdminLayout />
+              </RoleBaseRoutes>
+            </PrivateRoutes>
+          }
+        >
+          <Route index element={<AdminDashboard />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="students" element={<StudentManagement />} />
+          <Route path="parents" element={<ParentManagement />} />
+          <Route path="profile" element={<UserProfile />} />
+        </Route>
 
-                {/* Manager Routes */}
-                <Route
-                    path="/manager"
-                    element={
-                        <PrivateRoutes>
-                            <RoleBaseRoutes requiredRole={["MANAGER"]}>
-                                <ManagerLayout />
-                            </RoleBaseRoutes>
-                        </PrivateRoutes>
-                    }
-                >
-                    <Route index element={<ManagerDashboard />} />
-                    <Route path="students" element={<StudentList />} />
-                    <Route path="health-reports" element={<HealthReports />} />
-                    <Route
-                        path="vaccination-campaigns"
-                        element={<VaccinationCampaigns />}
-                    />
-                    <Route path="vaccination" element={<VaccineManagement />} />
-                    <Route
-                        path="health-checkup-campaigns"
-                        element={<HealthCheckupCampaigns />}
-                    />
-                    <Route path="alerts-events" element={<AlertsAndEvents />} />
-                    <Route path="profile" element={<UserProfile />} />
-                </Route>
+        {/* Manager Routes */}
+        <Route
+          path="/manager"
+          element={
+            <PrivateRoutes>
+              <RoleBaseRoutes requiredRole={["MANAGER"]}>
+                <ManagerLayout />
+              </RoleBaseRoutes>
+            </PrivateRoutes>
+          }
+        >
+          <Route index element={<ManagerDashboard />} />
+          <Route path="students" element={<StudentList />} />
+          <Route path="health-reports" element={<HealthReports />} />
+          <Route
+            path="vaccination-campaigns"
+            element={<VaccinationCampaigns />}
+          />
+          <Route path="vaccination" element={<VaccineManagement />} />
+          <Route
+            path="health-checkup-campaigns"
+            element={<HealthCheckupCampaigns />}
+          />
+          <Route path="alerts-events" element={<AlertsAndEvents />} />
+          <Route path="profile" element={<UserProfile />} />
+        </Route>
 
-                {/* User Routes */}
-                <Route
-                    path="/user"
-                    element={
-                        <PrivateRoutes>
-                            <RoleBaseRoutes requiredRole={["PARENT"]}>
-                                <User />
-                            </RoleBaseRoutes>
-                        </PrivateRoutes>
-                    }
-                >
-                    <Route index element={<Navigate to="home" />} />
-                    <Route path="home" element={<Homepage />} />
-                    <Route path="about" element={<About />} />
-                    <Route path="services" element={<Services />} />
-                    <Route path="doctors" element={<Doctors />} />
-                    <Route path="health-profile" element={<HealthProfile />} />
-                    <Route path="medicine-info" element={<MedicineInfo />} />
-                    <Route
-                        path="consent-forms"
-                        element={<VaccineConsentForm />}
-                    />
-                    <Route
-                        path="health-checkup-results"
-                        element={<HealthCheckupResults />}
-                    />
-                    <Route
-                        path="medical-schedule"
-                        element={<MedicalSchedule />}
-                    />
-                    <Route
-                        path="vaccination-history"
-                        element={<VaccinationHistory />}
-                    />
-                    <Route path="profile" element={<UserProfile />} />
-                    <Route path="medical-events" element={<MedicalEvents />} />
-                </Route>
-            </Routes>
-        </Router>
-    );
+        {/* User Routes */}
+        <Route
+          path="/parent"
+          element={
+            <PrivateRoutes>
+              <RoleBaseRoutes requiredRole={["PARENT"]}>
+                <User />
+              </RoleBaseRoutes>
+            </PrivateRoutes>
+          }
+        >
+          <Route index element={<Navigate to="home" />} />
+          <Route path="home" element={<Homepage />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="doctors" element={<Doctors />} />
+          <Route path="health-profile" element={<HealthProfile />} />
+          <Route path="medicine-info" element={<MedicineInfo />} />
+          <Route path="consent-forms" element={<VaccineConsentForm />} />
+          <Route
+            path="health-checkup-results"
+            element={<HealthCheckupResults />}
+          />
+          <Route path="medical-schedule" element={<MedicalSchedule />} />
+          <Route path="vaccination-history" element={<VaccinationHistory />} />
+          <Route path="profile" element={<UserProfile />} />
+          <Route path="medical-events" element={<MedicalEvents />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
